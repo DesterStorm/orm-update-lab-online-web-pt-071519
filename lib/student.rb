9fad5de5fa42191e8695cd5735c1136c7567b78e
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-10-01 13:19:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,8 @@ class Student
       INSERT INTO students (name, grade)
       VALUES (?, ?)
       SQL
-      DB[:conn].execute()
+      DB[:conn].execute(sql, self.name, self.grade)
+      
   end
 
 end
