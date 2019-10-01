de672a0cbe288da942e7641318506abe9cd4ffc3
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-10-01 13:36:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,9 +53,9 @@ class Student
   end
   
   def self.new_from_db(row)
-    id = [0]
-    name = [1]
-    grade = [2]
+    id = row[0]
+    name = row[1]
+    grade = row[2]
     self.new(id, name, grade)
   end
 
