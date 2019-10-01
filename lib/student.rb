773c93a36ebfb4ecce4eedc738ea6936881e3c15
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-10-01 13:29:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,5 +46,10 @@ class Student
     student.save
     student
   end
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end
