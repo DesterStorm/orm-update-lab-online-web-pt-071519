aa97d90fc5a2bcbd6c9feea8414d05fa8e9f5794
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-10-01 13:14:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,19 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
 
   def initialize(id = nil, name, grade)
     @id = id 
     @name = name 
     @grade = grade
+  end
+  
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
 
   def self.create_table
